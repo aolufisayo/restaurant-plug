3cf68c8dfd10f0fd746076f4e09a810f9b1802ba
aolufisayo <phissycodes@hotmail.com>
2021-11-18 00:22:02 +0000

    setup retrofit and hilt

---
+++ app/src/main/java/com/phissy/restaurantplug/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.phissy.restaurantplug.di
+
+import com.phissy.restaurantplug.common.Constants
+import com.phissy.restaurantplug.data.RestaurantService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesRestaurantService(): RestaurantService {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RestaurantService::class.java)
+    }
+}
